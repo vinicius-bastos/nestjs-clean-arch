b232f921a1e3942fdcbf98d1816c27fe4b9e7c3c
vinicius.cbastos <vinicius.cbastos@aec.com.br>
2024-08-29 15:07:30 -0300

    first commit

---
+++ src/projects/entities/project.entity.ts
@@ -0,0 +1,42 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+export enum ProjectStatus {
+  PENDING = 'pending',
+  ACTIVE = 'active',
+  CANCELLED = 'cancelled',
+  COMPLETED = 'completed',
+}
+
+@Entity()
+export class Project {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ nullable: true, type: 'datetime' })
+  started_at : Date | null;
+
+  @Column({ nullable: true, type: 'datetime' })
+  cancelled_at: Date | null;
+
+  @Column({ nullable: true, type: 'datetime' })
+  forecasted_at: Date | null;
+
+  @Column({ type: 'simple-enum' })
+  status: ProjectStatus;
+
+  constructor(props: {
+    name: string;
+    description: string;
+    stardet_at?: Date | null;
+    cancelled_at?: Date | null;
+    forecasted_at?: Date | null;
+  },
+  id?: string,
+  ) {
+    Object.assign(this, props);
+    this.id = id ?? crypto.randomUUID();
+  }
+}
